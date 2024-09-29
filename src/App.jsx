685b75c1cb528f4f04fecbee54e29c3b156d8b8b
Parsa_Shahsavan <shahsavanparsa4@gmail.com>
2024-09-29 21:09:28 +0330

    insert state to track the height, weight, and BMI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 
 function App() {
+
+  const [weight,setWeight] = useState(0)
+  const [height,setHeight] = useState(0)
+  const [bmi,setBMI] = useState(null)
+
+  const calculateBmi =()=>{
+    const heightInMeter = height / 100
+    const bmiValue = (weight / (heightInMeter * heightInMeter)).toFixed(2)
+    setBMI(bmiValue)
+  }
+
   return(
   <div>
-    
+    {/*  */}
   </div>
 ) ;
 }
