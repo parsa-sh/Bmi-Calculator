808d4b4484df0d284e30dcc2cec845207fd71cb5
Parsa_Shahsavan <shahsavanparsa4@gmail.com>
2024-09-29 23:42:37 +0330

    create UI components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,51 @@
+import {
+  Button,
+  Container,
+  HStack,
+  Input,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { useState } from "react";
+import "./App.css"
 
 function App() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBMI] = useState(null);
 
-  const [weight,setWeight] = useState(0)
-  const [height,setHeight] = useState(0)
-  const [bmi,setBMI] = useState(null)
+  const calculateBmi = () => {
+    const heightInMeter = height / 100;
+    const bmiValue = (weight / (heightInMeter * heightInMeter)).toFixed(2);
+    setBMI(bmiValue);
+  };
 
-  const calculateBmi =()=>{
-    const heightInMeter = height / 100
-    const bmiValue = (weight / (heightInMeter * heightInMeter)).toFixed(2)
-    setBMI(bmiValue)
-  }
-
-  return(
-  <div>
-    {/*  */}
-  </div>
-) ;
+  return (
+    <div className="mainDivider">
+      <Container bgColor="white" border="2px solid grey" borderRadius="18px" padding="40px">
+        <Text textAlign="center" fontWeight="600" fontSize="48px">BMI Calculator</Text>
+        <VStack marginTop="50px">
+          <HStack>
+            <Text fontWeight="500">Height (cm)</Text>
+            <Input placeholder="Enter your height" border="2px solid #b8b8b8" />
+          </HStack>
+          <HStack marginTop="20px">
+            <Text fontWeight="500">Weight (kg)</Text>
+            <Input placeholder="Enter your weight"  border="2px solid #b8b8b8"/>
+          </HStack>
+          <Button marginTop="30px" border="4px solid #b8b8b8" borderRadius="12px" padding="20px" fontWeight="700">Calculate BMI</Button>
+          <HStack marginTop="30px">
+            <Text fontWeight="600" fontSize="24px">BMI :</Text>
+            <Text fontSize="24px">34.7</Text>
+          </HStack>
+          <HStack marginTop="12px">
+            <Text fontWeight="600" fontSize="24px">Result :</Text>
+            <Text fontSize="24px">Fat</Text>
+          </HStack>
+        </VStack>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 // import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { ChakraProvider , ColorModeScript } from "@chakra-ui/react";
-import theme from "./theme.js"
+import { ChakraProvider } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <ChakraProvider theme={theme}>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+  <ChakraProvider>
     <App />
   </ChakraProvider>
   // </StrictMode>
