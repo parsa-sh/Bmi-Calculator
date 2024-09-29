d7e57f6c8316b48b279e02bd2d9d699a926048b4
Parsa_Shahsavan <shahsavanparsa4@gmail.com>
2024-09-30 00:08:32 +0330

    make components show the results

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   const [height, setHeight] = useState(0);
   const [bmi, setBMI] = useState(null);
 
+
   const calculateBmi = () => {
     const heightInMeter = height / 100;
     const bmiValue = (weight / (heightInMeter * heightInMeter)).toFixed(2);
@@ -27,16 +28,16 @@ function App() {
         <VStack marginTop="50px">
           <HStack>
             <Text fontWeight="500">Height (cm)</Text>
-            <Input placeholder="Enter your height" border="2px solid #b8b8b8" />
+            <Input type="number"  placeholder="Enter your height" border="2px solid #b8b8b8" onChange={(e)=>setHeight(e.target.value)}/>
           </HStack>
           <HStack marginTop="20px">
             <Text fontWeight="500">Weight (kg)</Text>
-            <Input placeholder="Enter your weight"  border="2px solid #b8b8b8"/>
+            <Input type="number"  placeholder="Enter your weight"  border="2px solid #b8b8b8" onChange={(e)=>setWeight(e.target.value)}/>
           </HStack>
-          <Button marginTop="30px" border="4px solid #b8b8b8" borderRadius="12px" padding="20px" fontWeight="700">Calculate BMI</Button>
+          <Button marginTop="30px" border="4px solid #b8b8b8" borderRadius="12px" padding="20px" fontWeight="700" onClick={calculateBmi}>Calculate BMI</Button>
           <HStack marginTop="30px">
             <Text fontWeight="600" fontSize="24px">BMI :</Text>
-            <Text fontSize="24px">34.7</Text>
+            {bmi && <HStack><Text>Your BMI is</Text><Text fontWeight="700" fontSize="18px" bg="#b3b3b387" padding="1px 5px" borderRadius="7px">{bmi}</Text></HStack>}
           </HStack>
           <HStack marginTop="12px">
             <Text fontWeight="600" fontSize="24px">Result :</Text>
